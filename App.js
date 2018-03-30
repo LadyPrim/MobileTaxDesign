c5bf5a18074e918ef475636ae49e1ed3a14591fd
Sofia Villar <sofvillar@gmail.com>
2018-03-30 20:34:12 +0200

    Form without boxes

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import {Component} from 'react';
+import {StackNavigator, } from 'react-navigation';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
 import t from 'tcomb-form-native';
+import {Font} from 'expo';
 
+// HomeScreen
+
+class HomeScreen extends Component {
+    static navigationsOptions = {
+        title: 'Home',
+    };
+
+    render(){
+        return(
+            <View>
+                <Text>Home Screen!</Text>
+                <Button
+                    title="Addresse hinzufügen"
+                    onPress={() => this.props.navigation.navigate('Address')}
+                />
+            </View>
+        );
+    }
+}
+
+// Adressformular mit Gebrauch von tcomb-form-native
 const Form = t.form.Form;
 
 const User = t.struct({
-    email: t.String,
-    username: t.maybe(t.String),
-    password: t.String,
-    terms: t.Boolean
+    address1: t.String,
+    address2: t.maybe(t.String),
+    plz: t.Number,
+    ort: t.String
 });
+
+t.form.Form.il8n = {
+    optional: '',
+    required: '*',
+};
 
 const formStyles = {
     ...Form.stylesheet,
@@ -22,8 +49,9 @@ const formStyles = {
     },
     controlLabel: {
         normal: {
-            color: 'blue',
-            fontSize: 18,
+            fontFamily: 'Roboto',
+            color: '#828282',
+            fontSize: 14,
             marginBottom: 7,
             fontWeight: '600'
         },
@@ -33,49 +61,121 @@ const formStyles = {
             fontSize: 18,
             marginBottom: 7,
             fontWeight: '600'
-        }
-    }
-}
+        },
+    },
+    textbox: {
+        normal: {
+            borderWidth: 0,
+            marginBottom: 0,
+        },
+    },
+    textboxView: {
+        normal: {
+            borderWidth: 0,
+            borderRadius: 0,
+            borderBottomWidth: 1,
+            marginBottom: 5,
+        },
+    },
+
+};
 
 const options = {
     fields: {
-        email: {
-            error: 'Without an email address how are you going to reset your password when you forget it?'
+        address1: {
+            label: 'Adresse 1',
+            error: 'Bitte eine gültige Strasse und Hausnummer angeben'
         },
-        password: {
-            error: 'Choose something you use on a dozen other sites or something you won\'t remember'
+        address2: {
+            label: 'Adresse 2',
         },
-        terms: {
-            label: 'Agree to Terms',
+        plz: {
+            label: 'PLZ',
+            error: '',
+        },
+        ort: {
+            label: 'Ort',
+            error: '',
         },
     },
     stylesheet: formStyles,
 };
 
-export default class App extends Component {
+
+
+class AddressScreen extends Component{
+    static navigationOptions = ({navigation}) => {
+        const {params} = navigation.state;
+
+        return{
+            title: params ? params.otherParam: ' Adresse ',
+        }
+    };
+
     handleSubmit = () => {
         const value = this._form.getValue();
         console.log('value: ', value);
-    }
-
+    };
     render() {
         return (
-            <View style={styles.container}>
-                <Form
-                    ref={c => this._form = c}
-                    type={User}
-                    options={options}
-                />
-                <Button
-                    title="Sign Up!"
-                    onPress={this.handleSubmit}
-                />
+            <View style={{backgroundColor: 'white'}}>
+                <View style={styles.container}>
+                    <Form
+                        ref={c => this._form = c}
+                        type={User}
+                        options={options}
+                    />
+
+                </View>
+                <View style={{backgroundColor: 'white'}}>
+                    <Button
+                        title="Sign Up!"
+                        onPress={this.handleSubmit}
+                    />
+                    <Button
+                        title="Go back"
+                        onPress={() => this.props.navigation.goBack()}
+                    />
+                </View>
             </View>
         );
     }
 }
 
+//Navigation
+const RootStack = StackNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+        },
+        Address: {
+            screen: AddressScreen
+        },
+    },
+    {
+        initialRouteName: 'Address'/*'Home'*/,
+    }
+);
+
+
+
+// AddressScreen
+export default class App extends Component {
+    componentDidMount() {
+        Font.loadAsync({
+            'open-sans-bold': require('./assets/fonts/Open_Sans/OpenSans-Bold.ttf'),
+        });
+    }
+
+    render(){
+        return <RootStack />;
+    }
+
+}
+
+// Style
 const styles = StyleSheet.create({
+
     container: {
         justifyContent: 'center',
         marginTop: 50,
